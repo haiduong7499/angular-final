cfaf61f70a459c7d25095aaf280d6303105b6efb
Mike0704 <haiduong7499@gmail.com>
2021-06-12 17:56:35 +0700

    validator submit form

--- src/app/component/create/create.component.ts
+++ src/app/component/create/create.component.ts
@@ -11,18 +11,21 @@ export class CreateComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   infoGame = this.fb.group({
-    "gameName":["",[Validators.required]],
-    "gameDescription":["",[Validators.required]],
-    "gamePrice":["",[Validators.required]],
-    "gamePlatform":["",[Validators.required]]
+    "gameName": ["", [Validators.required]],
+    "gameDescription": ["", [Validators.required]],
+    "gamePrice": ["", [Validators.required]],
+    "gamePlatform": ["", [Validators.required]]
   })
-  get form(){
+  get form() {
     return this.infoGame.controls;
   }
   ngOnInit(): void {
   }
 
-  onSubmit(){
-    console.log(this.infoGame.value);
+  onSubmit() {
+    if (this.infoGame.status === "INVALID") {
+      console.log('khong submit dc dau');
+    }
+    else { console.log(this.infoGame.value); }
   }
 }
